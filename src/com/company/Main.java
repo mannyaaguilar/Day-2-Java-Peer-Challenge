37df7a50ef1d8ebe737fe6a7e172f03769e1b712
Manny Aguilar <mannyaaguilar@yahoo.com>
2018-01-10 13:03:04 -0700

    Updated code with correct syntax

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,25 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        Scanner anything = new Scanner(System.in);
-        System.out.println("Enter Your Password");
-        String inputFromUser = anything.nextLine();
-
-        if (inputFromUser.equals("password"))
-        {
-            System.out.println("Access Granted");}
-        else
-            System.out.println("Denied");}
+        String password = "bacon";
+        System.out.println("Enter your password");
+        Scanner sc = new Scanner(System.in);
+        String enteredPassword;
+        enteredPassword = sc.nextLine();
+        if (enteredPassword.equals(password)) {
+            System.out.println("Gained Access");
+        } else {
+            System.out.println("Bzzzz! WRONG!");
+        }
+    }
+// Bad code!
+//        Scanner anything = new Scanner(System.in);
+//        System.out.println("Enter Your Password");
+//        String inputFromUser = anything.nextLine();
+//
+//        if (inputFromUser.equals("password"))
+//        {
+//            System.out.println("Access Granted");}
+//        else
+//            System.out.println("Denied");}
 }
